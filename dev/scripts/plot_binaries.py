ad9f3b6969bf88ec35b7e6f06921fbf2b7cc2ab1
Alessandro Pasqui <alessandro.pasqui@college-de-france.fr>
2024-04-16 16:10:48 +0200

    correcting small bug for plot binaries

--- dev/scripts/plot_binaries.py
+++ dev/scripts/plot_binaries.py
@@ -8,18 +8,16 @@ path = str(input('drag/drop folder: '))[:-1] + '/'
 
 for dt in range(int((len(os.listdir(path[:-1]))-4)/3)):
 
-    print(dt)
+    vertTable = np.load(path + str(dt) + '_vertTable.npy')
+    faceTable = np.load(path + str(dt) + '_faceTable.npy')
+    heTable = np.load(path + str(dt) + '_heTable.npy')
 
-    # vertTable = np.load(path + str(dt) + '_vertTable.npy')
-    # faceTable = np.load(path + str(dt) + '_faceTable.npy')
-    # heTable = np.load(path + str(dt) + '_heTable.npy')
-    #
-    # n_cells = len(faceTable)
-    # L_box = np.sqrt(n_cells)
-    #
-    # plot_periodic_voronoi(vertTable,
-    #                       faceTable,
-    #                       heTable,
-    #                       L_box=L_box, multicolor=True, lines=True,
-    #                       vertices=False, path=path, name=dt, save=True, show=False)
+    n_cells = len(faceTable)
+    L_box = np.sqrt(n_cells)
+
+    plot_periodic_voronoi(vertTable,
+                          faceTable,
+                          heTable,
+                          L_box=L_box, multicolor=True, lines=True,
+                          vertices=False, path=path, name=dt, save=True, show=False)
 
